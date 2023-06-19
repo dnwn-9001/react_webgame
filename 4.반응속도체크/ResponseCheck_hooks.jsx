d5fdf851201fc0947560a0075e6adcaf84877644
dnwn-9001 <j10511@hanmail.net>
2023-06-19 23:31:37 +0900

    반응속도체크 hooks로구현(+useRef)

---
+++ 4.반응속도체크/ResponseCheck_hooks.jsx
@@ -0,0 +1,63 @@
+import React, { useState, useRef } from "react";
+
+const ResponseCheck = () => {
+  const [state, setState] = useState("waiting");
+  const [message, setMessage] = useState("클릭해서 시작하세요.");
+  const [result, setResult] = useState([]);
+  const timeout = useRef(null);
+  const startTime = useRef();
+  const endTime = useRef();
+
+  const onClickScreen = () => {
+    if (state === "waiting") {
+      setState("ready");
+      setMessage("초록색이 되면 클릭하세요.");
+
+      timeout.current = setTimeout(() => {
+        setState("now");
+        setMessage("지금 클릭");
+        startTime.current = new Date();
+      }, Math.floor(Math.random() * 1000) + 2000); // 2~3초 랜덤
+    } else if (state === "ready") {
+      clearTimeout(timeout.current);
+      // 성급하게 클릭
+      setState("waiting");
+      setMessage("너무 성급하시군요! 초록색이 된 후에 클릭하세요.");
+    } else if (state === "now") {
+      endTime.current = new Date();
+      // 반응속도 체크
+      setState("waiting");
+      setMessage("클릭해서 시작하세요.");
+      setResult((prevResult) => {
+        return [...prevResult, endTime.current - startTime.current];
+      });
+    }
+  };
+
+  const onReset = () => {
+    setResult([]);
+  };
+
+  const renderAverage = () => {
+    return result.length !== 0 ? (
+      <>
+        <div>
+          평균 시간 :{result.reduce((a, c) => a + c) / result.length}
+          ms
+        </div>
+        <button onClick={onReset}>리셋</button>
+      </>
+    ) : null;
+  };
+
+  return (
+    <>
+      <div id="screen" className={state} onClick={onClickScreen}>
+        {message}
+      </div>
+      {renderAverage()}
+    </>
+  );
+};
+
+export default ResponseCheck;
--- 4.반응속도체크/client.jsx
+++ 4.반응속도체크/client.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDom from "react-dom";
-import ResponseCheck from "./ResponseCheck";
+// import ResponseCheck from "./ResponseCheck";
+import ResponseCheck from "./ResponseCheck_hooks";
 
 ReactDom.render(<ResponseCheck />, document.querySelector("#root"));
