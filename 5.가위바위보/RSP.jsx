42005a94f6b9091d1aa7d6a4d870f966e4564033
dnwn-9001 <j10511@hanmail.net>
2023-06-30 00:49:15 +0900

    가위바위보 클래스형 컴포넌트 완성

--- 5.가위바위보/RSP.jsx
+++ 5.가위바위보/RSP.jsx
@@ -1,5 +1,23 @@
 import React, { Component } from "react";
 
+const rspCoords = {
+  바위: 0,
+  가위: "-142px",
+  보: "-284px",
+};
+
+const scores = {
+  가위: 1,
+  바위: 0,
+  보: -1,
+};
+
+const computerChoice = (imgCoord) => {
+  return Object.entries(rspCoords).find(function (v) {
+    return v[1] === imgCoord;
+  })[0];
+};
+
 class Rsp extends Component {
   state = {
     result: "",
@@ -7,9 +25,62 @@ class Rsp extends Component {
     score: 0,
   };
 
-  componentDidMount() {}
+  interval;
+
+  changHand = () => {
+    const { imgCoord } = this.state;
+    if (imgCoord === rspCoords.바위) {
+      this.setState({
+        imgCoord: rspCoords.가위,
+      });
+    } else if (imgCoord === rspCoords.가위) {
+      this.setState({
+        imgCoord: rspCoords.보,
+      });
+    } else if (imgCoord === rspCoords.보) {
+      this.setState({
+        imgCoord: rspCoords.바위,
+      });
+    }
+  };
+
+  componentDidMount() {
+    this.interval = setInterval(this.changHand, 100);
+  }
+
+  componentDidUpdate() {}
 
   componentWillUnmount() {}
+
+  onClickBtn = (choice) => {
+    const { imgCoord } = this.state;
+    clearInterval(this.interval);
+    const myScore = scores[choice];
+    const computerScore = scores[computerChoice(imgCoord)];
+    const diff = myScore - computerScore;
+    if (diff === 0) {
+      this.setState({
+        result: "비겼습니다!",
+      });
+    } else if ([-1, 2].includes(diff)) {
+      this.setState((prevState) => {
+        return {
+          result: "이겼습니다!",
+          score: prevState.score + 1,
+        };
+      });
+    } else {
+      this.setState((prevState) => {
+        return {
+          result: "졌습니다!",
+          score: prevState.score - 1,
+        };
+      });
+    }
+    setTimeout(() => {
+      this.interval = setInterval(this.changHand, 100);
+    }, 2000);
+  };
 
   render() {
     const { result, score, imgCoord } = this.state;
@@ -26,7 +97,7 @@ class Rsp extends Component {
             id="rock"
             className="btn"
             onClick={() => {
-              onClickBtn("바위");
+              this.onClickBtn("바위");
             }}
           >
             바위
@@ -35,7 +106,7 @@ class Rsp extends Component {
             id="scissor"
             className="btn"
             onClick={() => {
-              onClickBtn("가위");
+              this.onClickBtn("가위");
             }}
           >
             가위
@@ -44,7 +115,7 @@ class Rsp extends Component {
             id="paper"
             className="btn"
             onClick={() => {
-              onClickBtn("보");
+              this.onClickBtn("보");
             }}
           >
             보
