0d2d81c0385cbc43d8acb5710afeb2bfe8512300
dnwn-9001 <j10511@hanmail.net>
2023-07-05 19:31:46 +0900

    로또 추첨기 함수 컴포넌트로 구현

---
+++ 6.로또/LottoHooks.jsx
@@ -0,0 +1,72 @@
+import React, { useState, useRef, useEffect } from "react";
+import Ball from "./Ball";
+
+function getWinNumbers() {
+  console.log("getWinNumbers");
+  const candidate = Array(45)
+    .fill()
+    .map((v, i) => i + 1);
+  const shuffle = [];
+  while (candidate.length > 0) {
+    shuffle.push(
+      candidate.splice(Math.floor(Math.random() * candidate.length), 1)[0]
+    );
+  }
+  const bonusNumber = shuffle[shuffle.length - 1];
+  const winNumbers = shuffle.slice(0, 6).sort((p, c) => p - c);
+  return [...winNumbers, bonusNumber];
+}
+
+const LottoHooks = () => {
+  const [winNumbers, setWinNumbers] = useState(getWinNumbers());
+  const [winBalls, setWinBalls] = useState([]);
+  const [bonus, setBonus] = useState(null);
+  const [redo, setRedo] = useState(false);
+  const timeouts = useRef([]);
+
+  const runTimeouts = () => {
+    for (let i = 0; i < winNumbers.length - 1; i++) {
+      timeouts.current[i] = setTimeout(() => {
+        setWinBalls((winBalls) => [...winBalls, winNumbers[i]]);
+      }, (i + 1) * 1000);
+    }
+
+    timeouts.current[winNumbers.length - 1] = setTimeout(() => {
+      setBonus(winNumbers[winNumbers.length - 1]);
+      setRedo(true);
+    }, 7000);
+  };
+
+  useEffect(() => {
+    runTimeouts();
+    return () => {
+      timeouts.current.forEach((v) => {
+        clearTimeout(v);
+      });
+    };
+  }, [timeouts.current]);
+
+  const onClickRedo = () => {
+    setWinNumbers(getWinNumbers());
+    setWinBalls([]);
+    setBonus(null);
+    setRedo(false);
+    timeouts.current = [];
+  };
+
+  return (
+    <>
+      <div>당첨 숫자</div>
+      <div id="결과창">
+        {winBalls.map((v) => (
+          <Ball key={v} number={v} />
+        ))}
+      </div>
+      <div>보너스!</div>
+      {bonus && <Ball number={bonus} />}
+      <div>{redo && <button onClick={onClickRedo}>한 번 더!</button>}</div>
+    </>
+  );
+};
+
+export default LottoHooks;
--- 6.로또/client.jsx
+++ 6.로또/client.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDom from "react-dom/client";
-import Lotto from "./Lotto";
+// import Lotto from "./Lotto";
+import LottoHooks from "./LottoHooks";
 
-ReactDom.createRoot(document.querySelector("#root")).render(<Lotto />);
+ReactDom.createRoot(document.querySelector("#root")).render(<LottoHooks />);
