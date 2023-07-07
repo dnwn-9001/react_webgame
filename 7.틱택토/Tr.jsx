0bf755f0abc7ef3d2840879d4ea262ab7c6add25
dnwn-9001 <j10511@hanmail.net>
2023-07-07 23:29:10 +0900

    틱택토 구현 완료

--- 7.틱택토/Tr.jsx
+++ 7.틱택토/Tr.jsx
@@ -3,20 +3,23 @@ import Td from "./Td";
 
 const Tr = ({ rowData, rowIndex, dispatch }) => {
   return (
-    <tr>
-      {Array(rowData.length)
-        .fill()
-        .map((td, i) => (
-          <Td
-            rowIndex={rowIndex}
-            cellIndex={i}
-            cellData={rowData[i]}
-            dispatch={dispatch}
-          >
-            {""}
-          </Td>
-        ))}
-    </tr>
+    <tbody>
+      <tr>
+        {Array(rowData.length)
+          .fill()
+          .map((td, i) => (
+            <Td
+              key={i}
+              rowIndex={rowIndex}
+              cellIndex={i}
+              cellData={rowData[i]}
+              dispatch={dispatch}
+            >
+              {""}
+            </Td>
+          ))}
+      </tr>
+    </tbody>
   );
 };
 
